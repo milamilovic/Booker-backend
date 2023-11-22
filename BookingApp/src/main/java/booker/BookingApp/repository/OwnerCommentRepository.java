930e77c86630e35515ccb2a5ceca4c4a9562b348
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2023-11-22 23:50:04 +0100

    added owner comments

---
+++ BookingApp/src/main/java/booker/BookingApp/repository/OwnerCommentRepository.java
@@ -0,0 +1,17 @@
+package booker.BookingApp.repository;
+
+import booker.BookingApp.model.OwnerComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OwnerCommentRepository extends JpaRepository<OwnerComment, Long> {
+    public OwnerComment findOne(Long id);
+    public OwnerComment findOneByOwnerId(Long ownerId);
+    public OwnerComment findOneByGuestId(Long guestId);
+    public List<OwnerComment> getAll();
+    public List<OwnerComment> getAllForOwner(Long ownerId);
+    public OwnerComment create(OwnerComment ownerComment);
+    public OwnerComment update(OwnerComment ownerComment);
+    public void delete(Long id);
+}
