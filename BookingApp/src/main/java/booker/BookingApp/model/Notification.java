187a278a2254c08d08455935f6dc333204382dde
matejicdunja <dunja2002matejic@gmail.com>
2023-11-22 22:13:11 +0100

    deleting unnecessary getters and setters

--- BookingApp/src/main/java/booker/BookingApp/model/Notification.java
+++ BookingApp/src/main/java/booker/BookingApp/model/Notification.java
@@ -8,28 +8,4 @@ public @Data class Notification {
     private Date time;
     private String content;
     private String title;
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
