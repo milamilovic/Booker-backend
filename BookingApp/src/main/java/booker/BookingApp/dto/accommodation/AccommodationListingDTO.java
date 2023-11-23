fe9134c6ed58c87e57b6a1145a6bd576e7cbbd92
milamilovic <milamilovic03@gmail.com>
2023-11-23 13:42:52 +0100

    accommodation controller and service fixed - service returns dto-s

--- BookingApp/src/main/java/booker/BookingApp/dto/accommodation/AccommodationListingDTO.java
+++ BookingApp/src/main/java/booker/BookingApp/dto/accommodation/AccommodationListingDTO.java
@@ -15,7 +15,7 @@ public @Data class AccommodationListingDTO {
     private int totalPrice;
     private int pricePerDay;
 
-    public AccommodationListingDTO makeFromAccommodation(Accommodation accommodation) {
+    public static AccommodationListingDTO makeFromAccommodation(Accommodation accommodation) {
         AccommodationListingDTO listingDTO = new AccommodationListingDTO();
 
         listingDTO.id = accommodation.getId();
@@ -26,6 +26,20 @@ public @Data class AccommodationListingDTO {
         listingDTO.rating = accommodation.getRatings().get(0);
         listingDTO.pricePerDay = accommodation.getPrices().get(0).getCost();
         listingDTO.totalPrice = accommodation.getPrices().get(0).getCost();
+
+        return listingDTO;
+    }
+
+    public static AccommodationListingDTO makeFromWholeAccommodation(WholeAccommodationDTO a) {AccommodationListingDTO listingDTO = new AccommodationListingDTO();
+
+        listingDTO.id = a.getId();
+        listingDTO.title = a.getTitle();
+        listingDTO.description = a.getShortDescription();
+        listingDTO.image =a.getImages().get(0);
+        //TODO make getAverageRating, getTotalPrice and getPricePerDay methods in service
+        listingDTO.rating = a.getRatings().get(0);
+        listingDTO.pricePerDay = a.getPrices().get(0).getCost();
+        listingDTO.totalPrice = a.getPrices().get(0).getCost();
 
         return listingDTO;
     }
