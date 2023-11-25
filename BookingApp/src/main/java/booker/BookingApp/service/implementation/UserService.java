2662344902e01a974a5ea3b8befc698f45dbd80b
milamilovic <milamilovic03@gmail.com>
2023-11-25 14:06:00 +0100

    resolving merging conflicts

--- BookingApp/src/main/java/booker/BookingApp/service/implementation/UserService.java
+++ BookingApp/src/main/java/booker/BookingApp/service/implementation/UserService.java
@@ -1,33 +1,39 @@
 package booker.BookingApp.service.implementation;
 
 import booker.BookingApp.model.users.User;
-import booker.BookingApp.repository.UserRepository;
+//import booker.BookingApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
     public User findOne(Long id){
-        return userRepository.findById(id).orElseGet(null);
+        return new User();
+        //return userRepository.findById(id).orElseGet(null);
     }
 
     public List<User> findAll() {
-        return userRepository.findAll();
+        return new ArrayList<User>();
+
+        //return userRepository.findAll();
     }
 
 
     public void remove(Long id) {
-        userRepository.deleteById(id);
+
+        //userRepository.deleteById(id);
     }
 
     public User save(User user) {
-        return userRepository.save(user);
+        return user;
+        //return userRepository.save(user);
     }
 
 }
