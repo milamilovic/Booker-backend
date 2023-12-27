ba8881fb83f018a138d0ef528be1f45ba9ca08ca
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2023-12-27 01:52:14 +0100

    feature: create owner ratings

--- BookingApp/src/main/java/booker/BookingApp/dto/commentsAndRatings/CreateOwnerRatingDTO.java
+++ BookingApp/src/main/java/booker/BookingApp/dto/commentsAndRatings/CreateOwnerRatingDTO.java
@@ -10,19 +10,18 @@ public @Data class CreateOwnerRatingDTO {
     private Long ownerId;
     private Long guestId;
     private float rate;
-    private Date date;
+//    private Date date;
 
     public CreateOwnerRatingDTO() {
     }
 
     public CreateOwnerRatingDTO(OwnerRating ownerRating) {
-        this(ownerRating.getOwner().getId(), ownerRating.getGuest().getId(), ownerRating.getRate(), ownerRating.getDate());
+        this(ownerRating.getOwner().getId(), ownerRating.getGuest().getId(), ownerRating.getRate());
     }
 
-    public CreateOwnerRatingDTO(Long ownerId, Long guestId, float rate, Date date) {
+    public CreateOwnerRatingDTO(Long ownerId, Long guestId, float rate) {
         this.ownerId = ownerId;
         this.guestId = guestId;
         this.rate = rate;
-        this.date = date;
     }
 }
