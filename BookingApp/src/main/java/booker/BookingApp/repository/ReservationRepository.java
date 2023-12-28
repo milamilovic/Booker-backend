250c1a3440a719bc182697856898d4ec5ee4898a
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2023-12-28 01:32:16 +0100

    added check by date

--- BookingApp/src/main/java/booker/BookingApp/repository/ReservationRepository.java
+++ BookingApp/src/main/java/booker/BookingApp/repository/ReservationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    @Query("SELECT r FROM Reservation r WHERE r.guest.id = ?1 AND r.accommodation.owner_id = ?2 AND r.status != 'CANCELED'")
+    @Query("SELECT r FROM Reservation r WHERE r.guest.id = ?1 AND r.accommodation.owner_id = ?2 AND r.status != 'CANCELED' AND r.toDate < CURRENT DATE")
     public List<Reservation> findAllForGuest(Long guestId, Long ownerId);
 }
